b132f0eb5edc1eb5b71e0fa31bb0aa58aea53d9c
Vanshika Agarwal <vanshikaagarwal814@gmail.com>
2025-04-21 00:50:55 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -3,17 +3,30 @@ const express = require("express");
 const app = express();
 
 //handle the request
-app.use("/", (req, res) =>{
-    res.send("Namaste Vanshika");
+
+app.use("/user", (req, res) =>{
+    res.send("HAHAHAHAHAHHA");
 });
 
+// 'get' will only handle the GET call for user
+app.get("/user", (req, res) =>{
+    res.send({firstname: "Vanshika", lastname: "agarwal"});
+});
+
+app.post("/user", (req, res) =>{
+    //saving data to DB
+    res.send("Data successfully send ot database!");
+});
+
+app.delete("/user", (req, res) =>{
+    res.send("Deleted successfully");
+})
+
+// 'use' will match all the http method API call to /test
 app.use("/test", (req, res) =>{
     res.send("Hello from the server..");
 });
 
-app.use("/hello", (req, res) =>{
-    res.send("Hello hello hello");
-});
 
 // server listening
 app.listen(3000, () =>{
